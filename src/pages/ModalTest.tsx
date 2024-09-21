59c530f4b17de0799044312dae310d0a4fce7519
summermong <2514o_o@naver.com>
2024-09-22 03:04:35 +0900

    03-5 using daisyui

--- src/pages/ModalTest.tsx
+++ src/pages/ModalTest.tsx
@@ -1,8 +1,30 @@
-export default function CopyMe() {
+import { Subtitle } from '../components';
+import { Button, Modal, ModalAction, ModalContent } from '../theme/daisyui';
+import * as D from '../data';
+import { useState } from 'react';
+
+export default function ModalTest() {
+  const [isOpen, setIsOpen] = useState(true);
+  const closeClicked = () => setIsOpen(false);
+  const acceptClicked = () => setIsOpen(false);
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">Copy Me</h2>
-      <div className="mt-4"></div>
+      <h2 className="text-5xl font-bold text-center">ModalTest</h2>
+      <Modal open={isOpen}>
+        <ModalContent onCloseIconClicked={acceptClicked}>
+          <Subtitle>Modal</Subtitle>
+          <p className="mt-4 text-justify">{D.randomParagraphs()}</p>
+          <ModalAction>
+            <Button className="w-24 btn-primary btn-sm" onClick={closeClicked}>
+              Accept
+            </Button>
+            <Button className="w-24 btn-sm" onClick={closeClicked}>
+              Close
+            </Button>
+          </ModalAction>
+        </ModalContent>
+      </Modal>
     </section>
   );
 }
