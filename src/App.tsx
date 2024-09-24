7f4abe2a856470b4f784e1db28620cf4fc796d85
summermong <2514o_o@naver.com>
2024-09-24 15:52:26 +0900

    04-3 useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import Callback from './pages/Callback';
-import HighOrderCallback from './pages/HighOrderCallback';
-import UseOrCreateTest from './pages/UseOrCreateTest';
+import ArrayState from './pages/ArrayState';
+import BasicForm from './pages/BasicForm';
+import HigherOrderRadioInputTest from './pages/HigherOrderRadioInputTest';
+import InputTest from './pages/InputTest';
+import NumberState from './pages/NumberState';
+// import RadioInputTest from './pages/RadioInputTest';
+import ShowHideModal from './pages/ShowHideModal';
 
 export default function App() {
   return (
     <main>
-      <UseOrCreateTest />
-      <Callback />
-      <HighOrderCallback />
+      <NumberState />
+      <InputTest />
+      <ShowHideModal />
+      {/* <RadioInputTest /> */}
+      <HigherOrderRadioInputTest />
+      <BasicForm />
+      <ArrayState />
     </main>
   );
 }
